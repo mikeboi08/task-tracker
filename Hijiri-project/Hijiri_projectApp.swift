c8d318d84909834a77c2b2ee7a26c3abe21aaecf
Micah Howard <micahhoward99@gmail.com>
2023-11-13 13:54:41 -0500

    Initial Commit

---
+++ Hijiri-project/Hijiri_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hijiri_projectApp.swift
+//  Hijiri-project
+//
+//  Created by Micah Howard on 11/13/23.
+//
+
+import SwiftUI
+
+@main
+struct Hijiri_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
