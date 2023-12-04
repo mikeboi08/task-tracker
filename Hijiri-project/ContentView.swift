0b8afb505915d31f9384744428c892eaeff37784
Micah Howard <micahhoward99@gmail.com>
2023-12-03 22:07:27 -0500

    formatted

--- Hijiri-project/ContentView.swift
+++ Hijiri-project/ContentView.swift
@@ -1,116 +1,195 @@
-//
-//  ContentView.swift
-//  Hijiri-project
-//
-//  Created by Micah Howard on 11/13/23.
-//
-
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showButtonStatus  = false
-   
-    
-    let lightOn = Circle().fill(.red) //Image("diodeFlashing")
-    let lightOff = Circle().fill(.gray) //Image("diode")
-    @State private var lightIsON = true
-    
-    @State var counter = 0
-    @State var lightbool1 = false
-   
-   // let timer = Timer.scheduledTimer(withTimeInterval: 0.2, repeats: true)
-    
-    let timer1 = Timer.publish(every: 0.5, on: .main, in: .common).autoconnect()
-    //let timer2 = Timer.publish(every: 2, on: .main, in: .common).autoconnect()
-    // i love coding
+    @State private var showButtonStatus = false
+    @State private var counter = 0
+    @State private var lightState: [Bool] = [false, false, false, false, false]
+    @State private var inProgressButtonState = false
+    @State private var timers: [Timer?] = [nil, nil, nil, nil, nil]
+
+    let timerInterval = 10.0
+    let blinkInterval = 0.5
+    let timer1 = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+
     var body: some View {
-        
-        ZStack{
+        ZStack {
             Color(.white)
-            VStack{
+            VStack {
                 Spacer()
-                Text("Tasks").bold()
-                HStack {
-                    Toggle(isOn: $showButtonStatus){
-//                        Text("Toggle")
+
+                Text("Tasks")
+                    .font(.system(size: 24, weight: .bold))
+                    .foregroundColor(.blue)
+
+                HStack(spacing: 20) {
+                    VStack {
+                        Text("Power")
+                            .font(.system(size: 18, weight: .bold))
+                            .foregroundColor(.blue)
+                        Toggle(isOn: Binding(
+                            get: { self.showButtonStatus },
+                            set: { newValue in
+                                self.showButtonStatus = newValue
+                                if newValue {
+                                    self.resetLights()
+                                }
+                            }
+                        )) {
+                            EmptyView()
+                        }
+                        .padding()
+                        .frame(maxWidth: .infinity)
                     }
-                    .padding()
-                    // if lightIsOn, show lightOn, else show lightOff
-                    
-                    
-                    if showButtonStatus == true && lightbool1 == true && counter > 1 {
-                        lightOn
-                            //.resizable()
-                            .aspectRatio(contentMode: .fit)
-                    }else {lightOff
-                        //.resizable()
-                        .aspectRatio(contentMode: .fit)}
-                    
-                    
-                    if showButtonStatus == true && lightbool1 == true && counter > 2 {
-                        lightOn
-                           // .resizable()
-                            .aspectRatio(contentMode: .fit)
-                    }else {lightOff
-                        //.resizable()
-                        .aspectRatio(contentMode: .fit)}
-                    if showButtonStatus == true && lightbool1 == true && counter > 3{
-                        lightOn
-                           // .resizable()
-                            .aspectRatio(contentMode: .fit)
-                    }else {lightOff
-                        //.resizable()
-                        .aspectRatio(contentMode: .fit)}
-                    
-                    if showButtonStatus == true && lightbool1 == true && counter > 4{
-                        lightOn
-                           // .resizable()
-                            .aspectRatio(contentMode: .fit)
-                    }else {lightOff
-                        //.resizable()
-                        .aspectRatio(contentMode: .fit)}
-                    if showButtonStatus == true && lightbool1 == true && counter > 5{
-                        lightOn
-                            //.resizable()
-                            .aspectRatio(contentMode: .fit)
-                    }else {lightOff
-                        //.resizable()
-                        .aspectRatio(contentMode: .fit)}
-                    
-                   
-                   
+                    ForEach(0..<5, id: \.self) { index in
+                        VStack {
+                            Text("\(index + 1)")
+                                .font(.system(size: 18, weight: .bold))
+                                .foregroundColor(.blue)
+                            Circle()
+                                .fill(lightState[index] && showButtonStatus ? Color.green : Color.gray)
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 50, height: 50)
+                                .disabled(!showButtonStatus)
+                        }
+                        .frame(maxWidth: .infinity)
+                    }
                 }
-                
-                Button(action: {
-                    if counter < 6 {
-                        counter+=1
-                    } else{counter = 0}
-                }, label: {
-                    Image("button")
-                })
-                if showButtonStatus == true && lightbool1 == true && counter > 0{
-                    lightOn
-                        //.resizable()
-                        .frame(width: 50, height: 70, alignment:.center)
-                }else {lightOff
-                    //.resizable()
-                    .frame(width: 50, height: 70, alignment:.center)}
-                   
+
+                HStack(spacing: 20) {
+                    VStack {
+                        Text("In Progress")
+                            .font(.system(size: 16, weight: .bold))
+                            .foregroundColor(.blue)
+                            .padding(.vertical, 4)
+
+                        Circle()
+                            .fill((lightState[0] || inProgressButtonState) && showButtonStatus ? Color.green : Color.gray)
+                            .frame(width: 50, height: 70)
+                            .disabled(!showButtonStatus)
+                    }
+                    .frame(maxWidth: .infinity)
+
+                    VStack {
+                        Text("Start Next Task")
+                            .font(.system(size: 16, weight: .bold))
+                            .foregroundColor(.blue)
+                            .padding(.top, 4)
+
+                        Spacer()
+
+                        Button(action: {
+                            cycleLights()
+                        }, label: {
+                            Image("button")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                        })
+                        .padding(.bottom, 20)
+                        .disabled(!showButtonStatus)
+                    }
+                    .frame(maxWidth: .infinity)
+                }
+
                 Spacer()
             }
-            .onReceive(timer1, perform: { index in
-                lightbool1.toggle()
-            })
-            
-//            .onReceive(timer2, perform: { _ in
-//                showButtonStatus2.toggle()
-//            })
-            
-            
+        }
+        .onReceive(timer1) { _ in
+            if showButtonStatus {
+                for i in 0..<lightState.count {
+                    if lightState[i] {
+                        if let timer = timers[i], timer.isValid {
+                            // Do nothing, timer is still running
+                        } else {
+                            lightState[i] = false
+                            timers[i] = nil
+                        }
+                    }
+                }
+
+                inProgressButtonState = counter > 0
+            }
         }
     }
-}
-    #Preview {
-        ContentView()
+
+    func cycleLights() {
+        counter += 1
+
+        if counter > lightState.count {
+            // Stop the cycling after button 5 blinks
+            resetLights()
+            return
+        }
+
+        for i in 0..<lightState.count {
+            if let timer = timers[i], timer.isValid {
+                // Stop the flashing for the current button
+                timer.invalidate()
+                timers[i] = nil
+                lightState[i] = false
+            }
+
+            if counter - 1 == i {
+                // Make the current button solid green
+                lightState[i] = true
+
+                // Schedule a timer to stop the flashing after 10 seconds
+                timers[i] = Timer.scheduledTimer(withTimeInterval: timerInterval, repeats: false) { [self] _ in
+                    lightState[i] = false
+                    inProgressButtonState = counter > 0
+
+                    // Start blinking the next light only if counter <= 5
+                    if counter <= 5 {
+                        let nextIndex = (i + 1) % lightState.count
+
+                        // Check if it's the 1st button, and if so, set it to solid green immediately
+                        if nextIndex != 0 {
+                            startBlinking(index: nextIndex)
+                        }
+                    }
+                }
+
+                // If it's the 1st button, set it to solid green immediately
+                if i == 0 {
+                    lightState[i] = true
+                }
+            }
+        }
     }
 
+    // Function to start blinking for a given index
+    private func startBlinking(index: Int) {
+        if index == 4 {
+            // Blink continuously for the 5th button until the green button is pressed
+            timers[index] = Timer.scheduledTimer(withTimeInterval: blinkInterval, repeats: true) { [self] _ in
+                lightState[index].toggle()
+            }
+        } else {
+            // Blink other buttons
+            timers[index] = Timer.scheduledTimer(withTimeInterval: blinkInterval, repeats: true) { [self] _ in
+                lightState[index].toggle()
+            }
+        }
+    }
+
+    // Function to reset lights after button 5 blinks
+    private func resetLights() {
+        counter = 0
+
+        for i in 0..<lightState.count {
+            if let timer = timers[i], timer.isValid {
+                // Stop the flashing for the current button
+                timer.invalidate()
+            }
+            timers[i] = nil
+            lightState[i] = false
+        }
+
+        inProgressButtonState = false
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
